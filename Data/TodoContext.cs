0a53188d3bedc8772a2c11af6fd6322c9b081886
Shreya Goyal <shreya.goyal@lytx.com>
2024-09-05 15:05:29 +0530

    Add user login and registration with JWT-based authentication.

--- Data/TodoContext.cs
+++ Data/TodoContext.cs
@@ -8,5 +8,7 @@ namespace ToDoApi.Data
         public ToDoContext(DbContextOptions<ToDoContext> options) : base(options) { }
 
         public DbSet<ToDo> ToDos { get; set; }
+
+        public DbSet<User> Users { get; set; }
     }
 }
